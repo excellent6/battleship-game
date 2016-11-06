9e98672e9e1c9d445d84f9a9fc4128df0fa8cbad
excellent6 <thracefields@gmail.com>
2016-11-06 17:57:16 +0200

    Bulk changes

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
             for (j = 0; j < rows; j += 1) {
                 square = document.createElement('div');
                 square.classList.add('battleship__item');
-                square.id = 's' + i + j;
+                square.id = 'js-s' + i + j;
                 fragment.appendChild(square);
             }
         }
@@ -34,20 +34,21 @@
     function fireTorpedo(e) {
         var row, col;
         if (e.target !== e.currentTarget) {
-            row = e.target.id.substring(1, 2);
-            col = e.target.id.substring(2, 3);
+            row = e.target.id.substring(4, 5);
+            col = e.target.id.substring(5, 6);
+            console.log(col, row);
             if (matrix[row][col] === 0) {
-                e.target.style.background = '#bbb';
+                 e.target.classList.add('battleship__item--missed');
                 matrix[row][col] = 3;
             } else if (matrix[row][col] == 1) {
-                e.target.style.background = 'red';
+                e.target.classList.add('battleship__item--danger');
                 matrix[row][col] = 2;
                 counter += 1;
                 if (counter === 3) {
                     alert("All enemy battleships have been defeated! You win!");
                 }
             } else if (matrix[row][col] > 1 && matrix[row][col] !== 2) {
-                e.target.style.backgroundColor = 'yellowgreen';
+                e.target.classList.add('battleship__item--success');
             }
         }
         e.stopPropagation();
