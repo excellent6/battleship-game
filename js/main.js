276ea72f16b0170020a3eb5be3deda3be264ab41
excellent6 <thracefields@gmail.com>
2016-11-09 18:21:20 +0200

    Final changes

--- js/main.js
+++ js/main.js
@@ -1,96 +1,94 @@
-var cols = 5,
-    rows = 5,
-    hitCounter = 0,
-    triesCounter = 0,
-    tries = document.querySelector('.battleship__tries'),
-    hits = document.querySelector('.battleship__hits'),
-    container = document.querySelector('.battleship-container'),
-    board = document.querySelector('.battleship'),
-    menu = document.querySelector('.battleship-menu');
+(function () {
+    var container = document.querySelector('.battleship-container'),
+        menu = document.querySelector('.battleship-menu');
 
-var matrix = (function () {
-    var i, col,
-        row,
-        baseMatrix = [
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-        ];
+    var matrix = (function () {
+        var i, col,
+            row,
+            baseMatrix = [
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]
+            ];
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    for (i = 0; i < 3; i += 1) {
-        col = getRandomInt(0, 4);
-        row = getRandomInt(0, 4);
-        baseMatrix[col][row] = 1;
-    }
-
-    return baseMatrix;
-} ());
-
-var board = (function (cols, rows) {
-    var i,
-        j,
-        square,
-        container = document.createElement('div');
-
-
-    container.classList.add('battleship');
-
-    for (i = 0; i < cols; i += 1) {
-        for (j = 0; j < rows; j += 1) {
-            square = document.createElement('div');
-            square.classList.add('battleship__item');
-            square.id = 'js-s' + i + j;
-            container.appendChild(square);
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
-    }
 
-    return container;
-} (cols, rows));
+        for (i = 0; i < 3; i += 1) {
+            col = getRandomInt(0, 4);
+            row = getRandomInt(0, 4);
+            baseMatrix[col][row] = 1;
+        }
+
+        return baseMatrix;
+    } ());
+
+    var board = (function (cols, rows) {
+        var i,
+            j,
+            square,
+            container = document.createElement('div');
 
 
-container.appendChild(board);
+        container.classList.add('battleship');
 
-function fireTorpedo(e) {
-    var row, col,
-        menuStart = document.querySelector('.battleship-menu__start'),
-        menuEnd = document.querySelector('.battleship-menu__end'),
-        newGameButton = document.querySelector('.battleship__new-game');
-    if (e.target !== e.currentTarget) {
-        row = e.target.id.substring(4, 5);
-        col = e.target.id.substring(5, 6);
-        if (matrix[row][col] === 0) {
-            e.target.classList.add('battleship__item--missed');
-            triesCounter += 1;
-            tries.innerHTML = triesCounter;
-            matrix[row][col] = 3;
-        } else if (matrix[row][col] === 1) {
-            e.target.classList.add('battleship__item--danger');
-            matrix[row][col] = 2;
-            hitCounter += 1;
-            hits.innerHTML = hitCounter;
-            if (hitCounter === 3) {
-                menu.classList.remove('battleship--hidden');
-                board.classList.add('battleship--hidden');
+        for (i = 0; i < cols; i += 1) {
+            for (j = 0; j < rows; j += 1) {
+                square = document.createElement('div');
+                square.classList.add('battleship__item');
+                square.id = 'js-s' + i + j;
+                container.appendChild(square);
             }
-        } else if (matrix[row][col] > 1 && matrix[row][col] !== 2) {
-            e.target.classList.remove('battleship__item--missed');
-            e.target.classList.add('battleship__item--success');
         }
-    }
-    e.stopPropagation();
-}
 
-container.appendChild(board);
-board.addEventListener('click', fireTorpedo);
+        return container;
+    } (5, 5));
 
-container.addEventListener('click', function () {
-    // board.style.visibility = 'visible';
-});
+    container.appendChild(board);
+    
+    (function () {
+        var hitCounter = 0,
+            triesCounter = 0,
+            tries = document.querySelector('.battleship__tries'),
+            hits = document.querySelector('.battleship__hits');
+
+        function fireTorpedo(e) {
+            var row, col,
+                menuStart = document.querySelector('.battleship-menu__start'),
+                menuEnd = document.querySelector('.battleship-menu__end'),
+                newGameButton = document.querySelector('.battleship__new-game');
+            if (e.target !== e.currentTarget) {
+                row = e.target.id.substring(4, 5);
+                col = e.target.id.substring(5, 6);
+                if (matrix[row][col] === 0) {
+                    e.target.classList.add('battleship__item--missed');
+                    triesCounter += 1;
+                    tries.innerHTML = triesCounter;
+                    matrix[row][col] = 3;
+                } else if (matrix[row][col] === 1) {
+                    e.target.classList.add('battleship__item--danger');
+                    matrix[row][col] = 2;
+                    hitCounter += 1;
+                    hits.innerHTML = hitCounter;
+                    if (hitCounter === 3) {
+                        menu.classList.remove('battleship--hidden');
+                        board.classList.add('battleship--hidden');
+                    }
+                } else if (matrix[row][col] > 1 && matrix[row][col] !== 2) {
+                    e.target.classList.remove('battleship__item--missed');
+                    e.target.classList.add('battleship__item--success');
+                }
+            }
+            e.stopPropagation();
+        }
+
+        board.addEventListener('click', fireTorpedo);
+
+    } ());
+
+} ());
